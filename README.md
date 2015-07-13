747f571a2a13453944ff8e8873c99133c9920d80
Alexandre Gauthier <alexandre230@msn.com>
2015-07-13 12:23:43 +0200

    Unit tests are now functionals. Please add more and read the README!

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Natron-Tests
 Unit tests of Natron renders based on images diff using ImageMagick.
 
-To successfully run the tests you should have a fully-featured version of Natron (+bundled plug-ins) working as well as ImageMagick installed on your system with the *convert* command in your path.
+The initial test suite was written by Alexandre Gauthier-Foichat.
+
+The main-repository for Natron is [here](https://github.com/MrKepzie/Natron).
+
+To successfully run the tests you should have a fully-featured version of Natron (+bundled plug-ins) working as well as ImageMagick installed on your system with the *compare* command in your path.
 
 To run the tests, simply run:
 
     ./run-tests.sh <NATRON_RENDERER_BINARY_ABSOLUTE_FILE_PATH>
 
-Make sure that Natron is using the version of the plug-ins that you expect (whether they are system-wide or bundled).
+Make sure that Natron is using the version of the plug-ins that you expect (whether they are system-wide or bundled) and that it can have access to the OpenColorIO configuration files. (Should be located in ../Resources relative to the NatronRenderer binary)
 
 Adding a new test
 -----------------
@@ -21,21 +25,30 @@ to the .ntp file and refer to them correctly in the Reader node using relative p
     - Line 1: the filename of the Natron project.
     - Line 2: the frame-range to test against the reference images. For example if you want to test the project against all reference images going from 1 to 10 (included), use 1 10 
     - Line 3: The script-name of the node in the project which has produced the reference frames.
+    - Line 4: The file-extension of the reference images. This should be something that the Write node of Natron can output.
+    - Line 5: (optional) The quality level (0-100) of the image to write for compression methods that allow a variable amount of compression. The default value used will be 10.
 
 A correct configuration file could be as such:
 
     test.ntp
     1 1
     Blur1
+    jpg
+    10
 
-- Reference output images must be in the EXR file format without any compression and 32bit floating point data-type. There should be exactly the same amount of reference images than specified in the frame-range of the configuration file. 
-    They should be named *reference* in sequence, that is the Write node that produced them should have a filename such as **/path/to/reference#.exr**.
+- Reference output images must be in a lightweight file format (such as jpg) with bad quality compression. We do not really care about the quality since a compression applied on the similar image should yield the same result. Note that output images should tend to be relatively small. There should be exactly the same amount of reference images than specified in the frame-range of the configuration file. 
+    They should be named *reference* in sequence, that is the Write node that produced them should have a filename such as **/path/to/reference#.jpg**.
     The Write node that produced them should have had its parameter named "Format type" set to the value "Project format" (the default).
 
 - Optional data can be added in the directory for debugging purposes, like similar projects made on other softwares so that the user can compare the result amongst softwares.
+
+You can submit new tests with a [pull-request](https://github.com/MrKepzie/Natron-Tests/pulls) containing the new test sub-directory. It will be accepted if it is respecting the points above.
 
 
 Comparison
 -----------
 
-ImageMagick is used internally 
+ImageMagick is used internally to count different pixels with the following command-line:
+
+    compare -metric AE reference.jpg output.jpg diff.jpg
+
--- runTests.sh
+++ runTests.sh
@@ -7,23 +7,43 @@
 # Created by Alexandre GAUTHIER-FOICHAT
 # contact: immarespond at gmail dot com
 
+#WARNING: Make sure that the NatronRenderer binary has access to the OpenColorIO-Configs otherwise images written may have
+#a different color-space of their reference.
+#The warning "Attempt to read an OpenColorIO configuration but the configuration directory..." will be printed
+#when the OpenColorIO-Configs could not be found.
+
+TEST_DIRS="TestFrameBlend TestRetimeTransform TestTimeBlur"
 
 if [ $# != 1 ]; then
 	echo "Usage: $0 <absolute path to NatronRenderer binary>"
+	echo "Or $0 clean to remove any output images generated."
 	exit 1
 fi
 
 
 RENDERER="$1"
+if [ "$1" = "clean" ]; then
+	for t in $TEST_DIRS; do
+		cd $t
+		rm *output*.* &> /dev/null
+		rm *comp*.*  &> /dev/null
+		rm *.autosave  &> /dev/null
+		rm *.lock  &> /dev/null
+		cd ..
+	done
+	exit 0
+fi
+
 TMP_SCRIPT="tmpScript.py"
 WRITER_PLUGINID="fr.inria.openfx.WriteOIIO"
 WRITER_NODE_NAME="__script_write_node__"
-IMAGES_FILE_EXT="exr"
+DEFAULT_QUALITY="10"
 
-TEST_DIRS="TestFrameBlend TestRetimeTransform TestTimeBlur"
 
 for t in $TEST_DIRS; do
 	cd $t
+
+    echo "===================$t========================"
 	CONFFILE=$(find conf)
 	if [[ -z $CONFFILE ]]; then
 		echo "$t does not contain a configuration file, please see the README."
@@ -37,16 +57,27 @@ for t in $TEST_DIRS; do
 	FIRST_FRAME=$(echo $CONF | awk '{print $2;}')
 	LAST_FRAME=$(echo $CONF | awk '{print $3;}')
 	OUTPUTNODE=$(echo $CONF | awk '{print $4;}')
+    IMAGES_FILE_EXT=$(echo $CONF | awk '{print $5;}')
+    QUALITY=$(echo $CONF | awk '{print $6;}')
+    if [[ -z $QUALITY ]]; then
+        QUALITY=$DEFAULT_QUALITY
+    fi
 	touch $TMP_SCRIPT
+    echo "import sys" > $TMP_SCRIPT
 	echo "import NatronEngine" > $TMP_SCRIPT
 	
 #Create the write node
-	echo "writer = app.createNode(\"$WRITER_PLUGINID\")" >> $TMP_SCRIPT	
+	echo "writer = app.createNode(\"$WRITER_PLUGINID\")" >> $TMP_SCRIPT
+    echo "if not writer.setScriptName(\"$WRITER_NODE_NAME\"):" >> $TMP_SCRIPT
+    echo "    raise NameError(\"Could not set writer script-name to $WRITER_NODE_NAME, aborting\")" >> $TMP_SCRIPT
+    echo "    sys.exit(1)" >> $TMP_SCRIPT
+    echo "#We must do this to copy the parameters attributes of the node to \"writer\"" >> $TMP_SCRIPT
+    echo "writer = app.$WRITER_NODE_NAME" >> $TMP_SCRIPT
 	echo "inputNode = app.$OUTPUTNODE" >> $TMP_SCRIPT
 	echo "writer.connectInput(0, inputNode)" >> $TMP_SCRIPT
 	
-	echo "writer.filename.set(\"[Project]/output#.exr\")" >> $TMP_SCRIPT
-
+#Set the output filename
+	echo "writer.filename.set(\"[Project]/output#.$IMAGES_FILE_EXT\")" >> $TMP_SCRIPT
 
 #Set manual frame range
 	echo "writer.frameRange.set(2)" >> $TMP_SCRIPT
@@ -54,23 +85,39 @@ for t in $TEST_DIRS; do
 	echo "writer.lastFrame.set($LAST_FRAME)" >> $TMP_SCRIPT
 	
 #Set compression to none
-	echo "writer.compression.set(1)" >> $TMP_SCRIPT
+	echo "writer.quality.set($QUALITY)" >> $TMP_SCRIPT
 	
-	echo "writer.setScriptName(\"$WRITER_NODE_NAME\")" >> $TMP_SCRIPT
-	
-#Start rendering
-	echo "Starting NatronRenderer for $t..."
-	$RENDERER -w $WRITER_NODE_NAME $NATRONPROJ || exit 1
-	
+
+#Start rendering, silent stdout
+	FAIL=$($RENDERER -w $WRITER_NODE_NAME -l $CWD/$TMP_SCRIPT $NATRONPROJ > /dev/null)
+    if [ "$FAIL" = "1" ]; then
+        rm ofxTestLog.txt &> /dev/null
+        rm $TMP_SCRIPT
+        exit 1
+    fi
+
+    rm ofxTestLog.txt &> /dev/null
+
 #compare with ImageMagick
 	for i in $(seq $FIRST_FRAME $LAST_FRAME); do
-		PIXELS_COUNT=compare -metric AE reference$i.$IMAGES_FILE_EXT output$i.$IMAGES_FILE_EXT comp$i.$IMAGES_FILE_EXT
+		compare -metric AE reference$i.$IMAGES_FILE_EXT output$i.$IMAGES_FILE_EXT comp$i.$IMAGES_FILE_EXT &> res
+        PIXELS_COUNT="$(cat res)"
+        rm res
+
 		if [ "$PIXELS_COUNT" != "0" ]; then
 			echo "WARNING: $PIXELS_COUNT pixel(s) different for frame $i in $t"
+            FAIL="1"
 		fi
+#rm output$i.$IMAGES_FILE_EXT > /dev/null
+        rm comp$i.$IMAGES_FILE_EXT > /dev/null
 	done
+    if [ "$FAIL" != "1" ]; then
+        echo "Test $t passed."
+    fi
+    FAIL="0"
 	
-	rm $TMP_SCRIPT
-	
+    rm $TMP_SCRIPT || exit 1
+    rm -rf __pycache__ &> /dev/null
+
 	cd ..
 done
