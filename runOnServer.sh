955752157d101ddd5a942636295ddc2c2ddcf55a
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-02-02 13:24:30 +0100

    symlink plugins

--- runOnServer.sh
+++ runOnServer.sh
@@ -3,4 +3,8 @@ export FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf
 export LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:/opt/Natron-CY2015/lib:/opt/Natron-CY2015/ffmpeg-gpl/lib
 export PATH=/opt/Natron-CY2015/bin:$PATH
 
+if [ ! -L /usr/OFX/Plugins ]; then
+  ln -sf /opt/Natron-CY2015/Plugins /usr/OFX/Plugins
+fi
+
 COMPARE=`pwd`/compare.bin ./runTests.sh /opt/Natron-CY2015/bin/NatronRenderer
