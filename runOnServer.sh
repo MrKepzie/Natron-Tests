c2eb546bcb1f4ce775a1fc280f42808f679470bd
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-02-02 13:20:30 +0100

    fix server script

--- runOnServer.sh
+++ runOnServer.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:$PATH COMPARE=$(pwd)/compare.bin ./runTests.sh /opt/Natron-CY2015/bin/NatronRenderer
+export FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf 
+export LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:/opt/Natron-CY2015/lib:/opt/Natron-CY2015/ffmpeg-gpl/lib
+export PATH=/opt/Natron-CY2015/bin:$PATH
+
+COMPARE=`pwd`/compare.bin ./runTests.sh /opt/Natron-CY2015/bin/NatronRenderer
