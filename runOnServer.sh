76f956d89f54408bc910b2e9d01b9facb09f427a
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-03-01 11:44:11 +0100

    add missing dir on linux

--- runOnServer.sh
+++ runOnServer.sh
@@ -2,7 +2,7 @@
 export FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf 
 export LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:/opt/Natron-CY2015/lib:/opt/Natron-CY2015/ffmpeg-gpl/lib
 export PATH=/opt/Natron-CY2015/bin:$PATH
-mkdir -p ~/.cache/INRIA/Natron/{ViewerCache,DiskCache}
+mkdir -p ~/.cache/INRIA/Natron/{ViewerCache,DiskCache} /usr/OFX
 
 if [ ! -L /usr/OFX/Plugins ]; then
   ln -sf /opt/Natron-CY2015/Plugins /usr/OFX/Plugins
