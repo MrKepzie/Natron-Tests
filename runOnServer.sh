83a41948737bf9cad207a67841ab666e30702575
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-06-22 19:12:31 +0200

    kill if stalled

--- runOnServer.sh
+++ runOnServer.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+PID=$$
+
+# make kill bot
+KILLSCRIPT="/tmp/killbot$$.sh"
+cat << 'EOF' > "$KILLSCRIPT"
+#!/bin/sh
+PARENT=$1
+sleep 15m
+if [ "$PARENT" = "" ]; then
+  exit 1
+fi
+PIDS=`ps aux|awk '{print $2}'|grep $PARENT`
+if [ "$PIDS" = "$PARENT" ]; then
+  kill -9 $PARENT
+fi
+EOF
+chmod +x $KILLSCRIPT
+
 export FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf 
 export LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:/opt/Natron-CY2015/lib:/opt/Natron-CY2015/ffmpeg-gpl/lib
 export PATH=/opt/Natron-CY2015/bin:$PATH
@@ -11,6 +29,12 @@ if [ ! -L /opt/Natron-CY2015/Plugins/PyPlugs ]; then
   ln -sf /opt/Natron-CY2015/PyPlugs /opt/Natron-CY2015/Plugins/PyPlugs
 fi
 
+"$KILLSCRIPT" $PID &
+KILLBOT=$!
+
 date
 FFMPEG="/opt/Natron-CY2015/ffmpeg-gpl/bin/ffmpeg" COMPARE=`pwd`/compare.bin ./runTests.sh /opt/Natron-CY2015/bin/NatronRenderer
 date
+
+kill -9 $KILLBOT
+rm -f $KILLSCRIPT || true
