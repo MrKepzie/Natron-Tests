f7009bb3700c7ff4860f6bedea97befd42811de1
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-06-30 10:22:49 +0200

    fix ref images

--- TestCMD/script.sh
+++ TestCMD/script.sh
@@ -15,8 +15,8 @@ fi
 rm -f "$CWD"/{output*,comp*,res*}
 
 echo "===================$NAME========================"
-"$NATRON_BIN" -i Read1 "$CWD"/input1.png -w Write1 "$CWD"/output1.jpg 1-1 -s -l "$CWD"/script01.py "$CWD"/test.ntp > /dev/null 2>&1
-"$NATRON_BIN" -i Read2 "$CWD"/input2.png -w Write2 "$CWD"/output2.jpg 1-1 -s -l "$CWD"/script01.py "$CWD"/test.ntp > /dev/null 2>&1 
+"$NATRON_BIN" -i ReadOIIO1 "$CWD"/input1.png -w WriteOIIO1 "$CWD"/output1.jpg 1-1 -s -l "$CWD"/script01.py "$CWD"/test.ntp #> /dev/null 2>&1
+"$NATRON_BIN" -i ReadOIIO2 "$CWD"/input2.png -w WriteOIIO2 "$CWD"/output2.jpg 1-1 -s -l "$CWD"/script01.py "$CWD"/test.ntp #> /dev/null 2>&1 
 
 for i in $(seq 1 2); do
   "$COMPARE_BIN" -metric AE reference$i.$IMAGES_FILE_EXT output$i.$IMAGES_FILE_EXT comp$i.$IMAGES_FILE_EXT &> res$i
@@ -27,15 +27,15 @@ for i in $(seq 1 2); do
     echo "Test $i passed for TestCMD"
   fi
 done
-rm -f "$CWD"/{output*,comp*,res*}
+#rm -f "$CWD"/{output*,comp*,res*}
 exit
 
-"$NATRON_BIN" -c "qualityValue=10" -c "app.saveProject(\"$CWD/output.ntp\")" -w DefaultWrite1 -w DefaultWrite2 -o1 "$CWD"/output5.jpg 1-1 -o2 "$CWD"/output6.jpg 1-1 "$CWD"/script02.py > /dev/null 2>&1
+"$NATRON_BIN" -c "qualityValue=10" -c "app.saveProject(\"$CWD/output.ntp\")" -w DefaultWrite1 -w DefaultWrite2 -o1 "$CWD"/output5.jpg 1-1 -o2 "$CWD"/output6.jpg 1-1 "$CWD"/script02.py #> /dev/null 2>&1
 
 if [ ! -f "$CWD/output3.jpg" ] && [ ! -f "$CWD/output4.jpg" ] && [ ! -f "$CWD/output5.jpg" ] && [ ! -f "$CWD/output6.jpg" ] && [ ! -f "$CWD/output.ntp" ]; then
   echo "WARNING: TestCMD failed test 2"
 else
   echo "TestCMD passed test 2"
 fi
-rm -f "$CWD"/output*
+#rm -f "$CWD"/output*
 
--- TestCMD/script01.py
+++ TestCMD/script01.py
@@ -1,6 +1,6 @@
 import NatronEngine
 
-writer1 = app.Write1
-writer2 = app.Write2
+writer1 = app.WriteOIIO1
+writer2 = app.WriteOIIO2
 writer1.quality.set(10)
 writer2.quality.set(10)
