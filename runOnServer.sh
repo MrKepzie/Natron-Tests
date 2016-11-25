84ac2db874db4af4bfe94db9b8d6ef18b667d023
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-11-25 22:26:44 +0100

    minor

--- runOnServer.sh
+++ runOnServer.sh
@@ -21,6 +21,7 @@ export FONTCONFIG_PATH=/opt/Natron-CY2015/etc/fonts/fonts.conf
 export LD_LIBRARY_PATH=/opt/Natron-CY2015/gcc/lib64:/opt/Natron-CY2015/lib:/opt/Natron-CY2015/ffmpeg-gpl/lib:/opt/Natron-CY2015/magick7/lib
 export PATH=/opt/Natron-CY2015/bin:$PATH
 mkdir -p ~/.cache/INRIA/Natron/{ViewerCache,DiskCache} /usr/OFX
+export OCIO=/opt/Natron-CY2015/Resources/OpenColorIO-Configs/blender/config.ocio
 
 if [ ! -L /usr/OFX/Plugins ]; then
   ln -sf /opt/Natron-CY2015/OFX/Plugins /usr/OFX/Plugins
