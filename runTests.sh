890ff67f9bb282d98bbc26ecfab85a6403c3390b
Alexandre Gauthier <alexandre230@msn.com>
2017-03-16 11:04:10 +0100

    set -e -u -x

--- runTests.sh
+++ runTests.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of Natron <http://www.natron.fr/>,
+# Copyright (C) 2016 INRIA and Alexandre Gauthier
 #
-# Created by Alexandre GAUTHIER-FOICHAT
-# contact: immarespond at gmail dot com
+# Natron is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Natron is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Natron.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>
+# ***** END LICENSE BLOCK *****
 
-#WARNING: Make sure that the NatronRenderer binary has access to the OpenColorIO-Configs otherwise images written may have
-#a different color-space of their reference.
-#The warning "Attempt to read an OpenColorIO configuration but the configuration directory..." will be printed
-#when the OpenColorIO-Configs could not be found.
+set -e -u -x
 
 echo "*** Natron tests"
 echo "Environment:"
